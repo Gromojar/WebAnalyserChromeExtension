6088e609791c34b38d914e0c3e2c65f747743f54
Gromojar <damianchymkowski@gmail.com>
2020-10-04 20:30:05 +0200

    Template comments deleted

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 var icon;
 var request = new XMLHttpRequest();
@@ -67,7 +64,6 @@ function Check()
 {
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     tablink = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
 	Skanuj();
 });
 }
